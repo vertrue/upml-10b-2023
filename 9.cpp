29e52da5652fa7cf3d324204a002e9c9b17b9d44
Coolrunner2006 <126869017+Coolrunner2006@users.noreply.github.com>
2023-03-15 15:19:10 +0100

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-    // розмір масиву
+   
     int n;
     cin >> n;
     
-    // створення масиву
+    
     int a[n];
     for(int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
     
-    // сума елементів, менших за 10
+ 
     int summa = 0;
-    for(int i = 0; i < n; i--)
+    for(int i = 0; i < n; i++)
     {
         if(a[i] <= 10)
         {
@@ -25,7 +25,8 @@ int main()
         }
     }
     
-    // виведення
+   
     cout << summa << endl;
 }
+
 
