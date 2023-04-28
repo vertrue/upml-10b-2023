c7536381086f4025bd4da6efb8652ce8c8601f2b
juaanius <vanoua17@gmail.com>
2023-04-28 09:22:25 +0300

    python

--- 8.cpp
+++ 8.cpp
@@ -1,31 +1,8 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    // розмір масиву
-    int n;
-    cin >> n;
-    
-    // створення масиву
-    int a[n];
-    for(int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    
-    // сума елементів, які менші за 5
-    intl summa = 0
-    for(int i = 1; i < n; i++)
-    {
-        if(a[i] < 5)
-        {
-            summa += a[i];
-        }
-    }
-    
-    // виведення
-    cout << summa << endl;
-}
-
+n = int(input())
+for i in range(n):
+	a = input(i)
+	int Sum = 0
+for i in range(n):
+	if a < 5
+	Sum += a
+print (Sum)
