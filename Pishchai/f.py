2b74cd0f7b47e9c0ed18dc594bda9d81e7fb981f
Yevhen Vitruk <30557724+vertrue@users.noreply.github.com>
2023-05-12 09:36:59 +0300

    Update f.py

--- Pishchai/f.py
+++ Pishchai/f.py
@@ -1,5 +1,5 @@
 def v10(n, a):
-for i in range(n):
-    if a[i] <=10:
-        summa+=a[i]
-        return summa
+    for i in range(n):
+        if a[i] <=10:
+            summa+=a[i]
+    return summa
